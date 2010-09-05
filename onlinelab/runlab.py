66bce4fecbcfbc0aa9ee57a723b4b0380ef6d6a0
Sameer Regmi <regmisk@gmail.com>
2010-09-05 12:18:05 -0700

    Edit runlab.py to improve message

--- onlinelab/runlab.py
+++ onlinelab/runlab.py
@@ -7,7 +7,16 @@ def run_lab_new():
     path_backend = sagelocal + "/lib/python2.6/site-packages/onlinelab/bin/run-backend"
     path_frontend = sagelocal + "/lib/python2.6/site-packages/onlinelab/bin/run-frontend"
 
-    process_backend = Popen([path_backend,], stdout=PIPE)
-    process_frontend = Popen([path_frontend,], stdout=PIPE)
-    print "FEMhub Online Lab is running now. Press Ctrl+C to terminate the processes associated with it."
-    process_frontend.wait()
+    process_backend = Popen([path_backend,])
+    process_frontend = Popen([path_frontend,])
+    #process_backend = Popen([path_backend,], stdout=PIPE)
+    #process_frontend = Popen([path_frontend,], stdout=PIPE)
+
+    print "FEMhub Online Lab is running now. Point your browser to
+    http://localhost:9000/ to login."
+    print "Press Ctrl+c to terminate the processes associated with the FEMhub Online Lab
+    press Ctrl+c."
+
+    #process_frontend.wait()
+    process_frontend.communicate()[0]
+    process_frontend.communicate()[0]
